50d098cf6b9021799def17208c95aebe14c367f5
helis5 <koroznikita@gmail.com>
2026-04-05 16:32:48 +0300

    interface bug fix

--- TaskPlanner/obj/Debug/net10.0/TaskPlanner.AssemblyInfo.cs
+++ TaskPlanner/obj/Debug/net10.0/TaskPlanner.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskPlanner")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb9c01342a1d20fdee827b563537d093a992080d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21011bf774730e92652ca89496885bf28c8132bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskPlanner")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskPlanner")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
